9adfb0d118d1a3e35447f1816cca8cdec8ac1797
Hemant Sonar <87268828+Hemant2318@users.noreply.github.com>
2021-07-16 16:53:47 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/banking_m_connect/MainActivity.java
+++ app/src/main/java/com/example/banking_m_connect/MainActivity.java
@@ -5,10 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView customer,transaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        customer=findViewById(R.id.customer);
+        transaction=findViewById(R.id.Transactions);
+
+        customer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,user_list.class));
+            }
+        });
+
+        transaction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,history_list.class));
+            }
+        });
     }
 }
